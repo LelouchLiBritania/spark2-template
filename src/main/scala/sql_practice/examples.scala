41c921f1db50dd90943624c55f640b5aa5e1f9ea
LeloouchLiBritania <florent.geniet@laposte.net>
2020-01-06 14:55:43 +0100

    exercise 3 & 4

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -76,4 +76,115 @@ object examples {
 
     popOrdered.join(departements,Seq("Departement")).show
   }
+
+  def exec3(): Unit ={
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val sample_07 = spark.read
+      .option("delimiter", "\t").csv("data/input/sample_07")
+      .select($"_c0".as("id"),$"_c1".as("name"),$"_c2".as("number_of_workers"),$"_c3".as("mean_salary"))
+    sample_07.show
+
+    val sample_08 = spark.read
+      .option("delimiter", "\t").csv("data/input/sample_08")
+      .select($"_c0".as("id"),$"_c1".as("name"),$"_c2".as("number_of_workers"),$"_c3".as("mean_salary"))
+    sample_08.show
+
+    println("Find top salaries in 2007 which are above 100k.")
+
+    sample_07.where("mean_salary>100000").orderBy($"mean_salary".desc).show()
+
+    println("Find salary growth sorted from 2007-08.")
+
+    sample_07
+      .join(sample_08,Seq("id"))
+      .withColumn("growth",sample_08("mean_salary")-sample_07("mean_salary"))
+      .orderBy($"growth".desc)
+      .show()
+
+    println("Find job loss among the top earnings from 2007-08.")
+
+    sample_07.where("mean_salary>100000")
+      .join(sample_08,Seq("id"))
+      .withColumn("job_loss",sample_07("number_of_workers")-sample_08("number_of_workers"))
+      .orderBy($"job_loss".desc)
+      .where("job_loss > 0")
+      .where(sample_08("mean_salary")>100000)
+      .show()
+  }
+
+  def exec4(): Unit ={
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val toursDF = spark.read
+      .option("multiline", true)
+      .option("mode", "PERMISSIVE")
+      .json("data/input/tours.json")
+    toursDF.show
+
+    println("How many unique levels of difficulties?")
+
+    toursDF
+      .select($"tourDifficulty")
+      .distinct()
+      .show()
+
+    println("What is the min/max/average of tour prices?")
+
+    toursDF
+      .agg(min("tourPrice").as("Min"),max("tourPrice").as("Max"),avg("tourPrice").as("Average"))
+      .show()
+
+    println("What is the min/max/average of price for each level of difficulty?")
+
+    toursDF
+      .groupBy($"tourDifficulty")
+      .agg(min("tourPrice").as("Min"),max("tourPrice").as("Max"),avg("tourPrice").as("Average"))
+      .show()
+
+    println("What is the min/max/average of price and duration for each level of difficulty?")
+
+    toursDF
+      .groupBy($"tourDifficulty")
+      .agg(
+        min("tourPrice").as("Min_price"),
+        max("tourPrice").as("Max_price"),
+        avg("tourPrice").as("Average_price"),
+        min("tourLength").as("Min_length"),
+        max("tourLength").as("Max_length"),
+        avg("tourLength").as("Average_length"))
+      .show()
+
+    println("Display the top 10 \"tourTags\"")
+
+    toursDF
+      .select(explode($"tourTags"))
+      .groupBy($"col")
+      .count()
+      .orderBy($"count".desc)
+      .show(10)
+
+    println("Relationship between top 10 \"tourTags\" and \"tourDifficulty\"")
+
+    toursDF
+      .select(explode($"tourTags"), $"tourDifficulty")
+      .groupBy($"col", $"tourDifficulty")
+      .count()
+      .orderBy($"count".desc)
+      .show(10)
+
+    println("What is the min/max/average of price in \"tourTags\" and \"tourDifficulty\" relationship, sorted by average?")
+
+    toursDF
+      .select(explode($"tourTags"), $"tourDifficulty",$"tourPrice")
+      .groupBy($"col", $"tourDifficulty")
+      .agg(
+        min("tourPrice").as("Min"),
+        max("tourPrice").as("Max"),
+        avg("tourPrice").as("Average"))
+      .orderBy($"Average".desc)
+      .show()
+  }
 }
