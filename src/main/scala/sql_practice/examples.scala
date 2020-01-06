63b443e141861c0a1abed4746f8d5edddb685f4f
LeloouchLiBritania <florent.geniet@laposte.net>
2020-01-06 12:02:47 +0100

    exercise 2

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -21,6 +21,7 @@ object examples {
       .count()
     )
 
+
     toursDF
       .select(explode($"tourTags"), $"tourDifficulty")
       .groupBy($"col", $"tourDifficulty")
@@ -34,5 +35,45 @@ object examples {
       .show(20)
 
 
+  }
+
+  def exec2(): Unit ={
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val population = spark.read
+      /*.option("multiline", true)
+      .option("mode", "PERMISSIVE")*/
+      .json("data/input/demographie_par_commune.json")
+    population.show
+
+    println("How many inhabitants has France?")
+    population.select("Population")
+      .agg(sum("Population")
+      .as("Population"))
+      .show()
+
+    println("What are the top highly populated departments in France?")
+    population.select("Population","Departement")
+      .groupBy("Departement")
+      .agg(sum("Population")
+      .as("Population"))
+      .orderBy(desc("Population"))
+      .show()
+
+    println("What are the top highly populated departments' name in France?")
+    val popOrdered = population.select("Population","Departement")
+      .groupBy("Departement")
+      .agg(sum("Population")
+      .as("Population"))
+      .orderBy(desc("Population"))
+
+    val departements = spark.read
+      /*.option("multiline", true)
+      .option("mode", "PERMISSIVE")*/
+      .csv("data/input/departements.txt")
+      .select($"_c0".as("name"),$"_c1".as("Departement"))
+
+    popOrdered.join(departements,Seq("Departement")).show
   }
 }
